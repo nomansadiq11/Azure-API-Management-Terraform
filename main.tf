dd834c9928bb6b11d867969e4ae878eca8ba9984
Noman Sadiq <nomi.cs786@gmail.com>
2019-11-25 19:50:02 +0400

    api Managment

--- main.tf
+++ main.tf
@@ -8,3 +8,26 @@ resource "azurerm_resource_group" "APIManagment" {
     }
   
 }
+
+
+
+resource "azurerm_api_management" "test" {
+  name                = "paymentfacadeTest"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  publisher_name      = "My Company"
+  publisher_email     = "company@terraform.io"
+
+  sku_name = "Developer_1"
+
+  policy {
+    xml_content = <<XML
+    <policies>
+      <inbound />
+      <backend />
+      <outbound />
+      <on-error />
+    </policies>
+XML
+  }
+}
