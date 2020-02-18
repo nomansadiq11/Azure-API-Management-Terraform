debbfb21ff612a109b4131aec5d53dc1b3edfc19
Noman Sadiq <nomi.cs786@gmail.com>
2020-02-18 12:19:28 +0400

    import API

--- main.tf
+++ main.tf
@@ -9,14 +9,12 @@ resource "azurerm_resource_group" "APIManagment" {
   
 }
 
-
-
-resource "azurerm_api_management" "test" {
+resource "azurerm_api_management" "paymentfacadeTest" {
   name                = "paymentfacadeTest"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.APIManagment.name}"
-  publisher_name      = "My Company"
-  publisher_email     = "company@terraform.io"
+  publisher_name      = "OSN"
+  publisher_email     = "noman.sadiq@osn.com"
 
   sku_name = "Developer_1"
 
@@ -24,77 +22,21 @@ resource "azurerm_api_management" "test" {
 }
 
 
-resource "azurerm_storage_account" "sapffunc" {
-  name                     = "sapffunc"
-  resource_group_name      = "${azurerm_resource_group.APIManagment.name}"
-  location                 = "${var.location}"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "${var.tag}"
-  }
-
-}
-
-resource "azurerm_app_service_plan" "asppffunc" {
-  name                = "asppffunc"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.APIManagment.name}"
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-
-  tags = {
-    environment = "${var.tag}"
-  }
-}
-
-resource "azurerm_function_app" "AF_OsnCloudPaymentsProxy" {
-  name                      = "afpffunc"
-  location                  = "${var.location}"
-  resource_group_name       = "${azurerm_resource_group.APIManagment.name}"
-  app_service_plan_id       = "${azurerm_app_service_plan.asppffunc.id}"
-  storage_connection_string = "${azurerm_storage_account.sapffunc.primary_connection_string}"
-
-  tags = {
-    environment = "${var.tag}"
-  }
-}
-
-
-
-# resource "azurerm_api_management_api_operation" "example" {
-#   operation_id        = "user-delete"
-#   api_name            = "testapi"
-#   api_management_name = "${azurerm_api_management.test.name}"
-#   resource_group_name = "${azurerm_resource_group.APIManagment.name}"
-#   display_name        = "Delete User Operation"
-#   method              = "DELETE"
-#   url_template        = "/users/{id}/delete"
-#   description         = "This can only be done by the logged in user."
-
-#   response {
-#     status_code = 200
-#   }
-# }
 
 
 ## this is import the API from Json 
 
-# resource "azurerm_api_management_api" "example" {
-#   name                = "test-api"
-#   resource_group_name = "${azurerm_resource_group.APIManagment.name}"
-#   api_management_name = "${azurerm_api_management.test.name}"
-#   revision            = "1"
-#   display_name        = "Example API"
-#   path                = "example"
-#   protocols           = ["https"]
+resource "azurerm_api_management_api" "example" {
+  name                = "test-api"
+  resource_group_name = "${azurerm_resource_group.APIManagment.name}"
+  api_management_name = "${azurerm_api_management.paymentfacadeTest.name}"
+  revision            = "1"
+  display_name        = "Example API"
+  path                = "example"
+  protocols           = ["https"]
 
-#   import {
-#     content_format = "swagger-link-json"
-#     content_value  = "http://conferenceapi.azurewebsites.net/?format=json"
-#   }
-# }
+  import {
+    content_format = "swagger-link-json"
+    content_value  = "https://paypaltestapi.osn.com/swagger/v1/swagger.json"
+  }
+}
