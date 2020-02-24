69c7823ee2d6626d7ee3415d5a7bfbd5049537ed
Noman Sadiq <nomi.cs786@gmail.com>
2020-02-24 18:11:09 +0400

    remove api and added apg

--- main.tf
+++ main.tf
@@ -9,34 +9,34 @@ resource "azurerm_resource_group" "APIManagment" {
   
 }
 
-resource "azurerm_api_management" "paymentfacadeTest" {
-  name                = "paymentfacadeTest"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.APIManagment.name}"
-  publisher_name      = "OSN"
-  publisher_email     = "noman.sadiq@osn.com"
+# resource "azurerm_api_management" "paymentfacadeTest" {
+#   name                = "paymentfacadeTest"
+#   location            = "${var.location}"
+#   resource_group_name = "${azurerm_resource_group.APIManagment.name}"
+#   publisher_name      = "OSN"
+#   publisher_email     = "noman.sadiq@osn.com"
 
-  sku_name = "Developer_1"
+#   sku_name = "Developer_1"
 
   
-}
+# }
 
 
 
 
-## this is import the API from Json 
+# ## this is import the API from Json 
 
-resource "azurerm_api_management_api" "example" {
-  name                = "Payment-Facade-API"
-  resource_group_name = "${azurerm_resource_group.APIManagment.name}"
-  api_management_name = "${azurerm_api_management.paymentfacadeTest.name}"
-  revision            = "1"
-  display_name        = "Payment Facade API"
-  path                = "example"
-  protocols           = ["https"]
+# resource "azurerm_api_management_api" "example" {
+#   name                = "Payment-Facade-API"
+#   resource_group_name = "${azurerm_resource_group.APIManagment.name}"
+#   api_management_name = "${azurerm_api_management.paymentfacadeTest.name}"
+#   revision            = "1"
+#   display_name        = "Payment Facade API"
+#   path                = "example"
+#   protocols           = ["https"]
 
-  import {
-    content_format = "swagger-link-json"
-    content_value  = "https://paypaltestapi.osn.com/swagger/v1/swagger.json"
-  }
-}
+#   import {
+#     content_format = "swagger-link-json"
+#     content_value  = "https://paypaltestapi.osn.com/swagger/v1/swagger.json"
+#   }
+# }
